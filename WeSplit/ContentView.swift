b00bc99be9b93ff4ca15ddc0d7f6a982b31a6665
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-06 12:13:32 +0100

    updates text field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! FUCK YALL!!")
+            Text("Hello, world :D")
         }
         .padding()
     }
